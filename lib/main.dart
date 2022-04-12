568e331b4456c889b1f39768dc2857ba0a681bd6
developython14 <djmel1955@gmail.com>
2022-04-12 15:55:15 +0100

    change icon of the app

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,13 @@ import 'package:right/intro_screen/wait.dart';
 import 'package:right/signup_pages/signup.dart';
 import 'package:right/signup_pages/signup_mod.dart';
 import 'package:right/test.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() {
   runApp(
