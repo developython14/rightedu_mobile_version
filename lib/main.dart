6cb3973efa66cc4abfdc884499a76dafe4364162
developython14 <djmel1955@gmail.com>
2022-04-25 19:13:57 +0100

    work on api of signup moderator and experts

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ import 'package:right/signup_pages/signup_mod.dart';
 import 'package:right/test.dart';
 
 void main() {
-  runApp(
-      MaterialApp(title: 'Right Hacker', initialRoute: '/signupmod', routes: {
+  runApp(MaterialApp(title: 'Right Hacker', initialRoute: '/wait', routes: {
     '/login': (context) => login(),
     '/signup': (context) => signup(),
     '/signupmod': (context) => signupmod(),
