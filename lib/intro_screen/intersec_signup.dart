6c45b7b17619e8554adaef667c9a3225727258a1
developython14 <djmel1955@gmail.com>
2022-04-28 15:17:16 +0100

    work on adding validators on ui

--- lib/intro_screen/intersec_signup.dart
+++ lib/intro_screen/intersec_signup.dart
@@ -1,1 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+class intersignup extends StatefulWidget {
+  const intersignup({Key? key}) : super(key: key);
+
+  @override
+  State<intersignup> createState() => _intersignupState();
+}
+
+class _intersignupState extends State<intersignup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GFButton(
+                onPressed: () {},
+                text: "signup as student",
+                shape: GFButtonShape.pills,
+              ),
+              GFButton(
+                onPressed: () {},
+                text: "signup as moderator",
+                shape: GFButtonShape.pills,
+              ),
+              GFButton(
+                onPressed: () {},
+                text: "signup as professeur",
+                shape: GFButtonShape.pills,
+              ),
+            ]),
+      ),
+    );
+  }
+}
