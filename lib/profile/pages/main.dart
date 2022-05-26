79af8fcfbc263d52681e84a42279c5132ecdfc7c
developython14 <djmel1955@gmail.com>
2022-05-26 22:06:02 +0100

    work on ui of chat part

--- lib/profile/pages/main.dart
+++ lib/profile/pages/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class start extends StatefulWidget {
   const start({Key? key}) : super(key: key);
@@ -8,10 +9,39 @@ class start extends StatefulWidget {
 }
 
 class _startState extends State<start> {
+  List list = [
+    "Flutter",
+    "React",
+    "Ionic",
+    "Xamarin",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hena rah ndir dekhla te3 l application '),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          GFSearchBar(
+            searchList: list,
+            searchQueryBuilder: (query, list) {
+              return list;
+            },
+            overlaySearchListItemBuilder: (item) {
+              return Container(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  "mustapha",
+                  style: const TextStyle(fontSize: 18),
+                ),
+              );
+            },
+            onItemSelected: (item) {
+              setState(() {
+                print('$item');
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
