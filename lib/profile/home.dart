9aa4573ebf90ef69178d1367606307891f0cf8f3
developython14 <djmel1955@gmail.com>
2022-05-26 17:19:31 +0100

    work on bottomnavigation bottom

--- lib/profile/home.dart
+++ lib/profile/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:right/profile/pages/discover.dart';
+import 'package:right/profile/pages/main.dart';
+import 'package:right/profile/pages/messages.dart';
+import 'package:right/profile/pages/profile.dart';
+import 'package:right/profile/pages/notification.dart';
 
 class home extends StatefulWidget {
   const home({Key? key}) : super(key: key);
@@ -10,25 +15,26 @@ class home extends StatefulWidget {
 
 class _homeState extends State<home> {
   int selected_index = 0;
+  List texts = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('mustapha test'),
-      ),
-      body: Center(child: Text("doka rana f index$selected_index")),
-      bottomNavigationBar:ConvexAppBar(
-    items: [
-      TabItem(icon: Icons.home, title: 'Home'),
-      TabItem(icon: Icons.map, title: 'Discovery'),
-      TabItem(icon: Icons.add, title: 'Add'),
-      TabItem(icon: Icons.message, title: 'Message'),
-      TabItem(icon: Icons.people, title: 'Profile'),
-    ],
-    initialActiveIndex: selected_index,//optional, default as 0
-    onTap: _set_index,
-  )
-    );
+        appBar: AppBar(
+          title: Text('mustapha test'),
+        ),
+        body: Center(
+            child: Text("doka rana f index $texts.elementAt(selected_index)")),
+        bottomNavigationBar: ConvexAppBar(
+          items: [
+            TabItem(icon: Icons.home, title: 'Home'),
+            TabItem(icon: Icons.map, title: 'Discovery'),
+            TabItem(icon: Icons.add, title: 'Add'),
+            TabItem(icon: Icons.message, title: 'Message'),
+            TabItem(icon: Icons.people, title: 'Profile'),
+          ],
+          initialActiveIndex: selected_index, //optional, default as 0
+          onTap: _set_index,
+        ));
   }
 
   void _set_index(int value) {
