fa619261c0d6c229dd3e7743da116dcd2335334f
developython14 <djmel1955@gmail.com>
2022-05-26 20:44:11 +0100

    work on ui of chat part

--- lib/profile/pages/messages.dart
+++ lib/profile/pages/messages.dart
@@ -19,29 +19,6 @@ class _messagesState extends State<messages> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        GFSearchBar(
-          searchList: list,
-          searchQueryBuilder: (query, list) {
-            return list
-                .where(
-                    (item) => item.toLowerCase().contains(query.toLowerCase()))
-                .toList();
-          },
-          overlaySearchListItemBuilder: (item) {
-            return Container(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                item,
-                style: const TextStyle(fontSize: 18),
-              ),
-            );
-          },
-          onItemSelected: (item) {
-            setState(() {
-              print('$item');
-            });
-          },
-        ),
         Card(
           child: ListTile(
             leading: ClipRRect(
