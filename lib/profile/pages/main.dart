e9b9dd10ed40643d9688599f284e83414da452d1
developython14 <djmel1955@gmail.com>
2022-05-26 22:44:05 +0100

    work on ui of chat part

--- lib/profile/pages/main.dart
+++ lib/profile/pages/main.dart
@@ -15,6 +15,15 @@ class _startState extends State<start> {
     "Ionic",
     "Xamarin",
   ];
+  final List<String> imageList = [
+    "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/13/00/23/christmas-3015776_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2019/12/22/04/18/x-mas-4711785__340.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/22/07/09/spruce-1848543__340.jpg"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -22,7 +31,7 @@ class _startState extends State<start> {
         children: [
           Text("Hi Mustapha ? Lets",
               style:
-                  const TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           GFSearchBar(
             searchList: list,
             searchQueryBuilder: (query, list) {
@@ -43,9 +52,32 @@ class _startState extends State<start> {
               });
             },
           ),
-          Text("Explore Our Services:",
-              style:
-                  const TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Explore Our Services:",
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          GFCarousel(
+            items: imageList.map(
+              (url) {
+                return Container(
+                  margin: EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Image.network(url, fit: BoxFit.cover, width: 1000.0),
+                  ),
+                );
+              },
+            ).toList(),
+            onPageChanged: (index) {
+              setState(() {
+                index;
+              });
+            },
+          ),
         ],
       ),
     );
