2cfc4f43460e53a89cd8688633e38eb8f9dd8c77
developython14 <djmel1955@gmail.com>
2022-05-26 20:26:09 +0100

    work on ui of chat part

--- lib/profile/pages/messages.dart
+++ lib/profile/pages/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class messages extends StatefulWidget {
   const messages({Key? key}) : super(key: key);
@@ -8,10 +9,39 @@ class messages extends StatefulWidget {
 }
 
 class _messagesState extends State<messages> {
+  List list = [
+    "Flutter",
+    "React",
+    "Ionic",
+    "Xamarin",
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        GFSearchBar(
+          searchList: list,
+          searchQueryBuilder: (query, list) {
+            return list
+                .where(
+                    (item) => item.toLowerCase().contains(query.toLowerCase()))
+                .toList();
+          },
+          overlaySearchListItemBuilder: (item) {
+            return Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                item,
+                style: const TextStyle(fontSize: 18),
+              ),
+            );
+          },
+          onItemSelected: (item) {
+            setState(() {
+              print('$item');
+            });
+          },
+        ),
         Card(
           child: ListTile(
             leading: ClipRRect(
@@ -50,6 +80,126 @@ class _messagesState extends State<messages> {
             ),
             title: Text(
               'Mustapha paul',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/fares.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha hacker',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/moh.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha pirate',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/samir.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha paul',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/fares.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha hacker',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/fares.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha hacker',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/moh.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha pirate',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/samir.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha paul',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'ya kho arwah essye tji  is a makach kifach  line  and arwah ya kho rani lhenna'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/start_app/fares.jpg', width: 56.0),
+            ),
+            title: Text(
+              'Mustapha hacker',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             subtitle: Text(
