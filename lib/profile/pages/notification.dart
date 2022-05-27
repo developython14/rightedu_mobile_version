9caed99dfe289aeba71e8f0b5be0a43fbbb8afbd
developython14 <djmel1955@gmail.com>
2022-05-27 07:38:36 +0100

    work on ui of chat part

--- lib/profile/pages/notification.dart
+++ lib/profile/pages/notification.dart
@@ -11,16 +11,27 @@ class _notificationState extends State<notification> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Card(
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            color: Colors.red,
-            shadowColor: Colors.blue,
-            shape: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.white)),
-            child: Column(
-              children: [Text('mustapha belkassem00'), Text("belkassse")],
-            )));
+        child: Container(
+      width: 200,
+      height: 200,
+      child: Card(
+          margin: EdgeInsets.all(10),
+          elevation: 10,
+          color: Colors.red,
+          shadowColor: Colors.blue,
+          shape: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.white)),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.asset('assets/start_app/moh.jpg', width: 80.0),
+              ),
+              Text('mustapha belkassem00'),
+              Text("belkassse")
+            ],
+          )),
+    ));
   }
 }
