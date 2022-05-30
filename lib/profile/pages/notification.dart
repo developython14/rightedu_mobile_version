b1a9461571491bc6e69ef589b254aaef3906f95d
developython14 <djmel1955@gmail.com>
2022-05-30 15:07:43 +0100

    work on adding validators on ui

--- lib/profile/pages/notification.dart
+++ lib/profile/pages/notification.dart
@@ -11,11 +11,36 @@ class notification extends StatefulWidget {
 class _notificationState extends State<notification> {
   List cards = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   Widget my = Card(
-    color: Colors.cyan,
-    child: Column(children: [
+    elevation: 10,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20),
+    ),
+    shadowColor: Colors.grey,
+    child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Image.asset(
+          "assets/start_app/moh.jpg",
+          height: 80.0,
+          width: 80.0,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text("Belkassem Mustapha", style: TextStyle(fontWeight: FontWeight.bold)),
+      SizedBox(
+        height: 10,
+      ),
       Text('software developper'),
-      Text("hacker ethical "),
-      Text("belkassemi")
+      SizedBox(
+        height: 10,
+      ),
+      GFButton(
+        onPressed: () {},
+        text: "contact",
+        shape: GFButtonShape.pills,
+      ),
     ]),
   );
   @override
