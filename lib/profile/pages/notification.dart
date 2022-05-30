1049c01f9b0aa13ae1acbe6a442bf7753f4f0dfa
developython14 <djmel1955@gmail.com>
2022-05-30 18:48:16 +0100

    work on ui experienece and user interface

--- lib/profile/pages/notification.dart
+++ lib/profile/pages/notification.dart
@@ -9,7 +9,6 @@ class notification extends StatefulWidget {
 }
 
 class _notificationState extends State<notification> {
-  List cards = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
   Widget my = Card(
     elevation: 10,
     shape: RoundedRectangleBorder(
@@ -49,21 +48,14 @@ class _notificationState extends State<notification> {
   );
   @override
   Widget build(BuildContext context) {
+    List<Widget> data = [my, my, my, my, my, my, my, my, my, my];
     return Center(
-      child: Container(
-          height: 225,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int index) {
-              return my;
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox(
-                width: 10,
-              );
-            },
-            itemCount: cards.length,
-          )),
+      child: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: data),
     );
   }
 }
