30ef534a8a2fda612c480c809650f8645480dd4c
developython14 <djmel1955@gmail.com>
2022-05-30 15:54:01 +0100

    work on adding validators on ui

---
+++ lib/profile/pages/compenant/profilecomponant.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
+class profileviewcard extends StatelessWidget {
+  const profileviewcard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      shadowColor: Colors.grey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              "assets/start_app/moh.jpg",
+              height: 80.0,
+              width: 80.0,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Belkassem Mustapha",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: 10,
+          ),
+          Text('software developper'),
+          SizedBox(
+            height: 10,
+          ),
+          GFButton(
+            onPressed: () {},
+            text: "contact",
+            shape: GFButtonShape.pills,
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/profile/pages/main.dart
+++ lib/profile/pages/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:right/profile/pages/compenant/profilecomponant.dart';
 
 class start extends StatefulWidget {
   const start({Key? key}) : super(key: key);
@@ -24,43 +25,7 @@ class _startState extends State<start> {
     "https://cdn.pixabay.com/photo/2016/11/22/07/09/spruce-1848543__340.jpg"
   ];
   List cards = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  Widget my = Card(
-    elevation: 10,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(20),
-    ),
-    shadowColor: Colors.grey,
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20.0),
-          child: Image.asset(
-            "assets/start_app/moh.jpg",
-            height: 80.0,
-            width: 80.0,
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text("Belkassem Mustapha",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        SizedBox(
-          height: 10,
-        ),
-        Text('software developper'),
-        SizedBox(
-          height: 10,
-        ),
-        GFButton(
-          onPressed: () {},
-          text: "contact",
-          shape: GFButtonShape.pills,
-        ),
-      ]),
-    ),
-  );
+  Widget my = profileviewcard();
 
   @override
   Widget build(BuildContext context) {
