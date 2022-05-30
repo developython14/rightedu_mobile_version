de5fd8fd38429638bfb850207a3a04775fe25a7b
developython14 <djmel1955@gmail.com>
2022-05-30 19:23:58 +0100

    work on ui experienece and user interface

--- lib/profile/pages/notification.dart
+++ lib/profile/pages/notification.dart
@@ -9,43 +9,46 @@ class notification extends StatefulWidget {
 }
 
 class _notificationState extends State<notification> {
-  Widget my = Card(
-    elevation: 10,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(20),
-    ),
-    shadowColor: Colors.grey,
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20.0),
-          child: Image.asset(
-            "assets/start_app/moh.jpg",
-            height: 80.0,
-            width: 80.0,
-          ),
+  Widget my = Container(
+      height: 250,
+      child: Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
         ),
-        SizedBox(
-          height: 10,
+        shadowColor: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset(
+                "assets/start_app/moh.jpg",
+                height: 80.0,
+                width: 80.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Belkassem Mustapha",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10,
+            ),
+            Text('software developper'),
+            SizedBox(
+              height: 10,
+            ),
+            GFButton(
+              onPressed: () {},
+              text: "contact",
+              shape: GFButtonShape.pills,
+            ),
+          ]),
         ),
-        Text("Belkassem Mustapha",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        SizedBox(
-          height: 10,
-        ),
-        Text('software developper'),
-        SizedBox(
-          height: 10,
-        ),
-        GFButton(
-          onPressed: () {},
-          text: "contact",
-          shape: GFButtonShape.pills,
-        ),
-      ]),
-    ),
-  );
+      ));
   @override
   Widget build(BuildContext context) {
     List<Widget> data = [my, my, my, my, my, my, my, my, my, my];
