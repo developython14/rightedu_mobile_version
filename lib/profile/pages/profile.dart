8df612746540802702c4fc4d84c19987999a2dd4
developython14 <djmel1955@gmail.com>
2022-06-04 19:35:42 +0100

    work on ui experienece and user interface

--- lib/profile/pages/profile.dart
+++ lib/profile/pages/profile.dart
@@ -12,62 +12,74 @@ class _profileState extends State<profile> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(25.0),
-            child: Image.asset(
-              "assets/start_app/fares.jpg",
-              height: 150.0,
-              width: 150.0,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text("mustapha"),
-              Text("free"),
-              Text("aboone"),
-            ],
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.person),
-              title: Text('edit profile'),
-              trailing: Icon(
-                Icons.arrow_forward_ios,
+            child: Padding(
+      padding: const EdgeInsets.only(top: 50, right: 10, left: 10),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(80.0),
+              child: Image.asset(
+                "assets/start_app/fares.jpg",
+                height: 150.0,
+                width: 150.0,
               ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('setting'),
-              trailing: Icon(Icons.more_vert),
+            SizedBox(
+              height: 15,
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.credit_card),
-              title: Text('payments cards'),
-              trailing: Icon(Icons.more_vert),
+            Text(
+              "Belkassem Mustapha",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.help_center),
-              title: Text('help and faq'),
-              trailing: Icon(Icons.more_vert),
+            Text(
+              "Software Engineer",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Edit Profile'),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              ),
             ),
-          ),
-        ])));
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Setting'),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.credit_card),
+                title: Text('Payments cards'),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.help_center),
+                title: Text('Help and faq'),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Logout'),
+              ),
+            ),
+          ]),
+    )));
   }
 }
