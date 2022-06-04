c71d1792daed3646688644807633b1e159ac56e6
developython14 <djmel1955@gmail.com>
2022-06-04 17:55:53 +0100

    work on ui experienece and user interface

--- lib/profile/pages/profile.dart
+++ lib/profile/pages/profile.dart
@@ -12,15 +12,25 @@ class _profileState extends State<profile> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: SingleChildScrollView(
-            child: Column(children: [
-      Image.asset("assets/start_app/fares.jpg"),
-      Row(
-        children: [
-          Text("mustapha"),
-          Text("belka"),
-          Text("aboone"),
-        ],
-      )
-    ])));
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(25.0),
+            child: Image.asset(
+              "assets/start_app/fares.jpg",
+              height: 200.0,
+              width: 200.0,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("mustapha"),
+              Text("belka"),
+              Text("aboone"),
+            ],
+          )
+        ])));
   }
 }
