5bc4e52b71e5971f0fc1377251aeae665b7d7844
developython14 <djmel1955@gmail.com>
2022-06-04 18:59:51 +0100

    work on ui experienece and user interface

--- lib/profile/pages/profile.dart
+++ lib/profile/pages/profile.dart
@@ -14,6 +14,7 @@ class _profileState extends State<profile> {
         child: SingleChildScrollView(
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(25.0),
@@ -24,13 +25,47 @@ class _profileState extends State<profile> {
             ),
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text("mustapha"),
               Text("free"),
               Text("aboone"),
             ],
-          )
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text('edit profile'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('setting'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.credit_card),
+              title: Text('payments cards'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.help_center),
+              title: Text('help and faq'),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+            ),
+          ),
         ])));
   }
 }
