6e56e8ed4f51d638018619571a3fc2bfa64760a9
developython14 <djmel1955@gmail.com>
2022-06-05 21:42:20 +0100

    work on ui experienece and user interface

--- lib/intro_screen/intersec_signup.dart
+++ lib/intro_screen/intersec_signup.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:right/loginpage/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class intersignup extends StatefulWidget {
+  var name = '';
+  var password = '';
   const intersignup({Key? key}) : super(key: key);
 
   @override
   State<intersignup> createState() => _intersignupState();
+}
+
+Future<void> getdata() async {
+  final prefs = await SharedPreferences.getInstance();
+  var nm = prefs.getString('name');
+  var pass = prefs.getString('password');
 }
 
 class _intersignupState extends State<intersignup> {
@@ -17,6 +27,7 @@ class _intersignupState extends State<intersignup> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
+              Text("name $name and pass is $password"),
               GFButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/signup');
