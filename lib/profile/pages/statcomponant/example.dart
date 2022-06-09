4acecc4a05da32756ad5dc3cc8c8b7e99f173bb9
developython14 <djmel1955@gmail.com>
2022-06-09 15:18:57 +0200

    work on ui of the full app

--- lib/profile/pages/statcomponant/example.dart
+++ lib/profile/pages/statcomponant/example.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class mus extends StatefulWidget {
+  const mus({Key? key}) : super(key: key);
+
+  @override
+  State<mus> createState() => _musState();
+}
+
+class _musState extends State<mus> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.network(
+            'https://evening-savannah-43647.herokuapp.com/static/img/image_slide/3.jpg'),
+      ),
+    );
+  }
+}
