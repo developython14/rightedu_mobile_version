8dca66652de7764f2c053dfdc1c85c0efad783f5
developython14 <djmel1955@gmail.com>
2022-06-09 19:13:05 +0200

    work on ui of the full app

--- lib/profile/pages/discover.dart
+++ lib/profile/pages/discover.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:right/profile/pages/compenant/post_widget.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class discover extends StatefulWidget {
   const discover({Key? key}) : super(key: key);
@@ -10,53 +12,41 @@ class discover extends StatefulWidget {
 }
 
 class _discoverState extends State<discover> {
+  List<Widget> posts = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getdata();
+  }
+
+  Future<void> getdata() async {
+    var test = Uri.parse(
+        'https://evening-savannah-43647.herokuapp.com/api/list_experts');
+    var response = await http.get(test);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      for (var i = 0; i < jsonResponse.length; i++) {
+        var mu = post_widget(jsonResponse[i]["name"],
+            jsonResponse[i]["country"], "assets/start_app/3.jpg");
+        posts.add(mu);
+        posts.add(
+          SizedBox(
+            height: 20,
+          ),
+        );
+      }
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: SingleChildScrollView(
       child: Column(
-        children: [
-          post_widget('mustapha Belkassem', 'assets/start_app/moh.jpg',
-              'assets/start_app/samir.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('mustapha fares', 'assets/start_app/0.jpg',
-              'assets/start_app/moh.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('taboukyout yassine', 'assets/start_app/1.jpg',
-              'assets/start_app/samir.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('mustapha ahmed', 'assets/start_app/2.jpg',
-              'assets/start_app/fares.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('mustapha hadjer', 'assets/start_app/3.jpg',
-              'assets/start_app/0.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('mustapha fares', 'assets/start_app/0.jpg',
-              'assets/start_app/1.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('taboukyout yassine', 'assets/start_app/1.jpg',
-              'assets/start_app/zaki12.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-          post_widget('mustapha ahmed', 'assets/start_app/2.jpg',
-              'assets/start_app/moh.jpg'),
-          SizedBox(
-            height: 20,
-          ),
-        ],
+        children: posts,
       ),
     ));
   }
