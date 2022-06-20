8540a6f1ed8b21b84cd2dbfabd7f3313c76a8651
developython14 <djmel1955@gmail.com>
2022-06-20 20:35:45 +0200

    work on card payments ui componant

--- lib/profile/navigation_pages/free.dart
+++ lib/profile/navigation_pages/free.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:awesome_card/awesome_card.dart';
 
 class free extends StatefulWidget {
   const free({Key? key}) : super(key: key);
@@ -14,7 +15,23 @@ class _freeState extends State<free> {
     return Scaffold(
       appBar: AppBar(title: Text('ALGERIA')),
       body: Column(
-        children: [],
+        children: [
+          CreditCard(
+              cardNumber: "77777 7879 4864 7854",
+              cardExpiry: "10/25",
+              cardHolderName: "Card Holder",
+              cvv: "356",
+              bankName: "Algeria Bank",
+              cardType: CardType
+                  .discover, // Optional if you want to override Card Type
+              showBackSide: false,
+              frontBackground: CardBackgrounds.black,
+              backBackground: CardBackgrounds.white,
+              showShadow: true,
+              textExpDate: 'Exp. Date',
+              textName: 'Name',
+              textExpiry: 'MM/YY'),
+        ],
       ),
     );
   }
