f7f89fd8e7fd7097674d97ebe647810594b94305
developython14 <djmel1955@gmail.com>
2022-06-25 14:07:57 +0200

    work on adding payment componant

--- lib/profile/navigation_pages/training.dart
+++ lib/profile/navigation_pages/training.dart
@@ -14,8 +14,65 @@ class _rightState extends State<right> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Algeria is gueriie'),
-    ));
+      appBar: AppBar(
+        title: Text('Algeria is gueriie'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Table(
+              border: TableBorder.all(),
+              children: [
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ]),
+                TableRow(children: [
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                  Text('Algeria is gueriie'),
+                ])
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
