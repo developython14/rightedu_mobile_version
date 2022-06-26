20c76badd798c9405028b701524b1bcfd37c6eb5
developython14 <djmel1955@gmail.com>
2022-06-26 18:37:55 +0200

    work on history pages profile

--- lib/profile/pages/edit_profile/hisprofile.dart
+++ lib/profile/pages/edit_profile/hisprofile.dart
@@ -10,81 +10,23 @@ class hisprofile extends StatefulWidget {
 class _hisprofileState extends State<hisprofile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('historique profile'),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(
-              'Services History',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Table(
-                border: TableBorder.all(),
-                children: [
-                  TableRow(children: [
-                    Text(
-                      'id',
-                    ),
-                    Text(
-                      'service name',
-                    ),
-                    Text(
-                      'service state',
-                    ),
-                    Text(
-                      'professor',
-                    ),
-                    Text(
-                      'prices',
-                    )
-                  ]),
-                  TableRow(children: [
-                    Text(
-                      'id',
-                    ),
-                    Text(
-                      'service name',
-                    ),
-                    Text(
-                      'service state',
-                    ),
-                    Container(
-                      color: Color.fromARGB(255, 243, 33, 121),
-                      height: 40,
-                    ),
-                    Text(
-                      'prices',
-                    )
-                  ]),
-                  TableRow(children: [
-                    Text(
-                      'id',
-                    ),
-                    Text(
-                      'service name',
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      height: 40,
-                    ),
-                    Icon(Icons.abc_sharp),
-                    Text(
-                      'prices',
-                    )
-                  ])
-                ],
-              ),
-            )
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            children: [
+              Icon(Icons.history),
+              Text('history'),
+            ],
+          ),
+          bottom: TabBar(tabs: [Text('packag'), Text('packag')]),
         ),
-      )),
+        body: Center(
+          child: TabBarView(children: [Text('algeria '), Text('france')]),
+        ),
+      ),
     );
   }
 }
