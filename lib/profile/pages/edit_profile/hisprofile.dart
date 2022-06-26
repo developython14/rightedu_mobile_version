b6b7c5e775b9623f834ded88733bdd6c5ac95038
developython14 <djmel1955@gmail.com>
2022-06-26 18:44:46 +0200

    work on history pages profile

--- lib/profile/pages/edit_profile/hisprofile.dart
+++ lib/profile/pages/edit_profile/hisprofile.dart
@@ -16,12 +16,49 @@ class _hisprofileState extends State<hisprofile> {
         appBar: AppBar(
           centerTitle: true,
           title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.history),
-              Text('history'),
+              Icon(
+                Icons.history,
+                size: 40,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'History',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
             ],
           ),
-          bottom: TabBar(tabs: [Text('packag'), Text('packag')]),
+          bottom: TabBar(tabs: [
+            Row(
+              children: [
+                Icon(
+                  Icons.cleaning_services,
+                  size: 30,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Services',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.podcasts,
+                  size: 30,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Posts',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          ]),
         ),
         body: Center(
           child: TabBarView(children: [Text('algeria '), Text('france')]),
