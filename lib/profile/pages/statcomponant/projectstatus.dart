b4d1c93252ef15416ca4d0497600fba4e60673fd
developython14 <djmel1955@gmail.com>
2022-06-28 21:32:14 +0200

    work on ui componant

--- lib/profile/pages/statcomponant/projectstatus.dart
+++ lib/profile/pages/statcomponant/projectstatus.dart
@@ -9,6 +9,9 @@ class projectstatus extends StatefulWidget {
 }
 
 class _projectstatusState extends State<projectstatus> {
+  List _processes = [];
+  int _processIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,88 +36,92 @@ class _projectstatusState extends State<projectstatus> {
       ),
       body: Column(
         children: [
-          StatusChange.tileBuilder(
-            theme: StatusChangeThemeData(
-              direction: Axis.vertical,
-              connectorTheme: ConnectorThemeData(space: 1.0, thickness: 1.0),
-            ),
-            builder: StatusChangeTileBuilder.connected(
-              itemWidth: (_) =>
-                  MediaQuery.of(context).size.width / _processes.length,
-              contentWidgetBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Text(
-                    'add content here',
-                    style: TextStyle(
-                      color: Colors
-                          .blue, // change color with dynamic color --> can find it with example section
-                    ),
-                  ),
-                );
-              },
-              nameWidgetBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    'your text ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: getColor(index),
-                    ),
-                  ),
-                );
-              },
-              indicatorWidgetBuilder: (_, index) {
-                if (index <= _processIndex) {
-                  return DotIndicator(
-                    size: 35.0,
-                    border: Border.all(color: Colors.green, width: 1),
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.green,
-                        ),
+          Container(
+            width: 200,
+            height: 300,
+            child: StatusChange.tileBuilder(
+              theme: StatusChangeThemeData(
+                direction: Axis.vertical,
+                connectorTheme: ConnectorThemeData(space: 1.0, thickness: 1.0),
+              ),
+              builder: StatusChangeTileBuilder.connected(
+                itemWidth: (_) =>
+                    MediaQuery.of(context).size.width / _processes.length,
+                contentWidgetBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      'add content here',
+                      style: TextStyle(
+                        color: Colors
+                            .blue, // change color with dynamic color --> can find it with example section
                       ),
                     ),
                   );
-                } else {
-                  return OutlinedDotIndicator(
-                    size: 30,
-                    borderWidth: 1.0,
-                    color: todoColor,
+                },
+                nameWidgetBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      'your text ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
                   );
-                }
-              },
-              lineWidgetBuilder: (index) {
-                if (index > 0) {
-                  if (index == _processIndex) {
-                    final prevColor = getColor(index - 1);
-                    final color = getColor(index);
-                    var gradientColors;
-                    gradientColors = [
-                      prevColor,
-                      Color.lerp(prevColor, color, 0.5)
-                    ];
-                    return DecoratedLineConnector(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: gradientColors,
+                },
+                indicatorWidgetBuilder: (_, index) {
+                  if (index <= _processIndex) {
+                    return DotIndicator(
+                      size: 35.0,
+                      border: Border.all(color: Colors.green, width: 1),
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                          ),
                         ),
                       ),
                     );
                   } else {
-                    return SolidLineConnector(
-                      color: getColor(index),
+                    return OutlinedDotIndicator(
+                      size: 30,
+                      borderWidth: 1.0,
+                      color: Colors.green,
                     );
                   }
-                } else {
-                  return null;
-                }
-              },
-              itemCount: _processes.length,
+                },
+                lineWidgetBuilder: (index) {
+                  if (index > 0) {
+                    if (index == _processIndex) {
+                      final prevColor = Colors.green;
+                      final color = Colors.green;
+                      var gradientColors;
+                      gradientColors = [
+                        prevColor,
+                        Color.lerp(prevColor, color, 0.5)
+                      ];
+                      return DecoratedLineConnector(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: gradientColors,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return SolidLineConnector(
+                        color: Colors.green,
+                      );
+                    }
+                  } else {
+                    return null;
+                  }
+                },
+                itemCount: _processes.length,
+              ),
             ),
           ),
         ],
