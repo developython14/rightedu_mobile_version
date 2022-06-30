972eed46a7545aca7d76fac3a66a33517f31a77b
developython14 <djmel1955@gmail.com>
2022-06-30 21:26:28 +0200

    record voics and open camera

--- lib/profile/pages/statcomponant/projectstatus.dart
+++ lib/profile/pages/statcomponant/projectstatus.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class mustaphaudio extends StatefulWidget {
   const mustaphaudio({Key? key}) : super(key: key);
@@ -69,6 +70,65 @@ class _mustaphaudioState extends State<mustaphaudio> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class mustapharecorder extends StatefulWidget {
+  @override
+  State<mustapharecorder> createState() => _mustapharecorderState();
+}
+
+class _mustapharecorderState extends State<mustapharecorder> {
+  FlutterSoundRecorder _myRecorder = FlutterSoundRecorder();
+  bool _mRecorderIsInited = false;
+  Future<void> record() async {
+    // Request Microphone permission if needed
+    PermissionStatus status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted)
+      throw RecordingPermissionException("Microphone permission not granted");
+    await _myRecorder.startRecorder(
+      toFile: 'yassietabou',
+      codec: Codec.aacADTS,
+    );
+  }
+
+  Future<void> stopRecorder() async {
+    await _myRecorder.stopRecorder();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _myRecorder.openAudioSession().then((value) {
+      setState(() {
+        _mRecorderIsInited = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _myRecorder.closeAudioSession();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('play'),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          Text('start record '),
+          ElevatedButton(onPressed: record, child: Text('start record')),
+          ElevatedButton(onPressed: stopRecorder, child: Text('Stop record'))
+        ],
+      )),
     );
   }
 }
