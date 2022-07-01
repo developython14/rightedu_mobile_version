39392c876f6cf9208b4f6b77677251ae069c31ee
developython14 <djmel1955@gmail.com>
2022-07-01 15:15:04 +0200

    intgrate video player in the app

--- lib/profile/pages/statcomponant/projectstatus.dart
+++ lib/profile/pages/statcomponant/projectstatus.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -11,9 +10,9 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
-  double wid = 200;
-  double hei = 1200;
-  String data = 'asse';
+  double wid = 400;
+  double hei = 850;
+  String data = 'free';
 
   @override
   void initState() {
@@ -22,7 +21,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     // Create and store the VideoPlayerController. The VideoPlayerController
     // offers several different constructors to play videos from assets, files,
     // or the internet.
-    _controller = VideoPlayerController.asset('assets/start_app/mustapha.mp4');
+    _controller = VideoPlayerController.network(
+        'https://www.fluttercampus.com/video.mp4');
 
     // Initialize the controller and store the Future for later use.
     _initializeVideoPlayerFuture = _controller.initialize();
@@ -44,44 +44,41 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     return Scaffold(
       // Use a FutureBuilder to display a loading spinner while waiting for the
       // VideoPlayerController to finish initializing.
-      body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          height: wid,
-          width: 1200,
-          child: GestureDetector(
-            onTap: () {
-              print('mustapha capteur');
-              setState(() {
-                wid = MediaQuery.of(context).size.width;
-                hei = MediaQuery.of(context).size.height;
-                print(hei);
-                data = 'belkassem';
-              });
-            },
-            child: FutureBuilder(
-              future: _initializeVideoPlayerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  // If the VideoPlayerController has finished initialization, use
-                  // the data it provides to limit the aspect ratio of the video.
-                  return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    // Use the VideoPlayer widget to display the video.
-                    child: VideoPlayer(_controller),
-                  );
-                } else {
-                  // If the VideoPlayerController is still initializing, show a
-                  // loading spinner.
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ),
+      body: Center(
+        child: FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the video.
+              return AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: GestureDetector(
+                    onTap: (() {
+                      print('https');
+                      setState(() {
+                        wid = 500;
+                        hei = 500;
+                      });
+                    }),
+                    child: Container(
+                        width: wid,
+                        height: hei,
+                        color: Colors.green,
+                        child: VideoPlayer(_controller))),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Wrap the play or pause in a call to `setState`. This ensures the
