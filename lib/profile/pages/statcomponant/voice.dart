e15b229418eeca9608241af060bf5646326f48e1
developython14 <djmel1955@gmail.com>
2022-07-06 22:12:29 +0200

    work on animation colors in flutter

--- lib/profile/pages/statcomponant/voice.dart
+++ lib/profile/pages/statcomponant/voice.dart
@@ -7,6 +7,42 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:flutter/services.dart';
 
+class authentifaction {
+  static final _auth = LocalAuthentication();
+
+  static Future<bool> authentifcate() async {
+    final is_avalaible = await _auth.canCheckBiometrics;
+    if (!is_avalaible) {
+      return false;
+    }
+    ;
+
+    try {
+      return await _auth.authenticate(localizedReason: 'scan you foot');
+    } on PlatformException catch (e) {
+      return false;
+    }
+  }
+}
+
+testava() async {
+  final _auth = LocalAuthentication();
+  final is_avalaible = await _auth.canCheckBiometrics;
+}
+
+testprint() async {
+  print('start correctly');
+  final _auth = LocalAuthentication();
+  try {
+    final bool didAuthenticate = await _auth.authenticate(
+        localizedReason: 'Please authenticate to show account balance',
+        options: const AuthenticationOptions(useErrorDialogs: false));
+    // ···
+  } on PlatformException catch (e) {
+    print(e);
+  }
+}
+
 class mustapha extends StatefulWidget {
   const mustapha({Key? key}) : super(key: key);
 
@@ -19,7 +55,13 @@ class _mustaphaState extends State<mustapha> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('new  york'),
+        title: Text('free'),
+      ),
+      body: Center(
+        child: Column(children: [
+          ElevatedButton(onPressed: testava, child: Text('test avalability ')),
+          ElevatedButton(onPressed: testprint, child: Text('test footprint '))
+        ]),
       ),
     );
   }
